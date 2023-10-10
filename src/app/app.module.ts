1b64add0a7ec93473780c7828785bf1485e22c76
komalgs11 <komalpatil26.kp@gmail.com>
2023-10-10 17:51:08 +0200

    added required angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,14 +16,21 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatListModule } from '@angular/material/list';
 import { FormsModule } from '@angular/forms';
 import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { MovieDescriptionComponent } from './movie-description/movie-description.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: UserProfileComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
 ];
 
@@ -35,6 +42,8 @@ const appRoutes: Routes = [
     MovieCardComponent,
     WelcomePageComponent,
     MovieDescriptionComponent,
+    UserProfileComponent,
+    NavBarComponent,
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
@@ -50,6 +59,10 @@ const appRoutes: Routes = [
     MatButtonModule,
     MatInputModule,
     MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTooltipModule,
+    MatProgressSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
