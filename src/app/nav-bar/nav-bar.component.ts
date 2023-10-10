27b100f60f5fec1ec92315bee59ca40f7e9033bf
komalgs11 <komalpatil26.kp@gmail.com>
2023-10-10 17:52:24 +0200

    added nav bar component

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.scss'],
+})
+export class NavBarComponent {
+  constructor(
+    private router: Router,
+    public fetchApiData: FetchApiDataService
+  ) {}
+
+  logoutUser(): void {
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    this.router.navigate(['welcome']);
+  }
+}
